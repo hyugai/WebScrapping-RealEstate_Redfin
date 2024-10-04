76a294fe9cda3faec8c93857be8cde906d9b0c92
huyle <huyle210504@gmail.com>
2024-10-04 22:13:02 +0700

    m

--- tests/url_scrapper.py
+++
@@ -1,7 +0,0 @@
-# libs
-import os, sys
-cwd = os.getcwd()
-os.chdir('src/'); path_to_src = os.getcwd()
-os.chdir(cwd)
-from _libs import *
-from _usr_libs import *
---
+++ tests/url_scrapper_exp.py
@@ -0,0 +1,9 @@
+# libs
+import os, sys
+cwd = os.getcwd()
+os.chdir('../src/'); path_to_src = os.getcwd()
+os.chdir(cwd)
+from _libs import *
+from _usr_libs import *
+
+# exp
