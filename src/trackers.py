5026545d181dd8b1cfa53663fe410a0ece038ce2
hyugai <huyle210504@gmail.com>
2024-10-04 14:47:16 +0700

    m

--- src/trackers.py
+++ src/trackers.py
@@ -13,7 +13,7 @@ class LogsTracker():
         with sqlite3.connect(self.path_to_db) as conn:
             cur = conn.cursor()
             cur.execute("CREATE TABLE IF NOT EXISTS logs(city UNIQUE TEXT, url TEXT, status INTEGER)")
-            cur.execute(f"INSERT OR REPLACE INTO logs VALUES('{city}', '{url}', {status})")
+            cur.execute("INSERT OR REPLACE INTO logs(city, url, status) VALUES(?, ?, ?)", (city, url, status))
             conn.commit(); cur.close()
 
     # update
@@ -84,4 +84,29 @@ class CityTracker():
             conn.commit()
 
 # class URL tracker
+class URLTracker():
+    def __init__(self, 
+                 path_to_db: str) -> None:
+        self.path_to_db = path_to_db
 
+    def insert(self, 
+               city: str, url: str, csv_download_link: str) -> None:
+        with sqlite3.connect(self.path_to_db) as conn:
+            cur = conn.cursor()
+            cur.execute("CREATE TABLE IF NOT EXISTS urls(city UNIQUE TEXT, url TEXT, csv_download_link TEXT)")
+            cur.execute(f"INSERT OR REPLACE INTO urls(city, url, csv_download_link) VALUES(?, ?, ?)", (city, url, csv_download_link))
+            conn.commit(); cur.close()
+
+    def retrive(self, csv_download_link_IS_NULL: bool=True) -> list[tuple]:
+        with sqlite3.connect(self.path_to_db) as conn:
+            cur = conn.cursor()
+            if csv_download_link_IS_NULL:
+                cur.execute("SELECT city, url FROM urls WHERE csv_download_link IS NULL")
+                rows = cur.fetchall(); conn.commit()
+
+                return rows
+            else:
+                cur.execute("SELECT city, csv_download_link FROM urls WHERE csv_download_link IS NOT NULL")
+                rows = cur.fetchall()
+
+                return rows
--- tests/logs_tracker.py
+++
@@ -1,6 +0,0 @@
-# libs
-import os, sys
-cwd = os.getcwd()
-os.chdir('../'); path_to_src = os.getcwd(); os.chdir(cwd)
-if path_to_src not in sys.path:
-    sys.path.append(path_to_src)
