ca882acf2e14f718385f930e0a081c22f160c3a9
hyugai <huyle210504@gmail.com>
2024-10-04 15:27:56 +0700

    m

--- tests/redfin_experiment.py
+++ tests/redfin_experiment.py
@@ -6,4 +6,12 @@ path_to_src = os.getcwd(); os.chdir(cwd)
 if path_to_src not in sys.path:
     sys.path.append(path_to_src)
 from _usr_libs import *
-from redfin_config import *
+from redfin_config import EMAIL, PASSWORD
+from redfin import RedfinHeadlessChromeBrowser
+
+# test
+print(cwd)
+os.chdir('tests/dbs'); download_dir = os.getcwd()
+os.chdir(cwd)
+redfin = RedfinHeadlessChromeBrowser(download_dir, EMAIL, PASSWORD)
+browser = redfin._set_up_browser()
