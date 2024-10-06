c7abe9c47cfb6540bfe0d3ce898d4733e12cbb4c
huyle <huyle210504@gmail.com>
2024-10-06 16:26:58 +0700

    m

--- jupyter/test.py
+++ jupyter/test.py
@@ -2,10 +2,11 @@ import os, sys
 
 # import src
 cwd = os.getcwd()
-os.chdir('../'); path_to_src = os.getcwd(); os.chdir(cwd)
+os.chdir('src/'); path_to_src = os.getcwd(); 
+os.chdir(cwd)
 if path_to_src not in sys.path:
     sys.path.append(path_to_src)
-from src.draft import *
+from draft import *
 
 url = 'https://www.redfin.com/city/30818/TX/Austin'
 eles = split_HomesData(url)
@@ -15,5 +16,5 @@ eles = [ele+'}' for ele in eles]
 # pattern = r':"[^"]+"[^,"]+"[^"]+"'
 for ele in eles:
     test = re.sub(r':"[^"]+"[^,"]+"[^"]+"', ':""', ele)
-    eval(test)
+    print(eval(test))
 # fix 2: try replace the group with desired value 
